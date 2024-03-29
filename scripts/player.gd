cd5c149fb76c17f44ca29fb2baf8c9129e8a6056
Bekz <bekzatkhan.maulen@gmail.com>
2024-03-29 15:09:28 +0000

    added new movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var movement_data : PlayerMovementData
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var air_jump = false
+
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 
@@ -11,6 +13,8 @@ func _physics_process(delta):
 	handle_jump()
 	var input_axis = Input.get_axis("move_left", "move_right")
 	handle_acceleration(input_axis, delta)
+	handle_air_acceleration(input_axis, delta)
+	handle_wall_jump()
 	apply_friction(input_axis, delta)
 	apply_air_resistance(input_axis, delta)
 	update_animations(input_axis)
@@ -24,12 +28,31 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("powerup"):
 		movement_data = load("res://scripts/resources/FastMovement.tres")
 
-
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * movement_data.gravity_scale * delta 
 
+func handle_wall_jump():
+	if is_on_wall():
+		if not is_on_floor():
+			air_jump = true
+			
+		var wall_normal = get_wall_normal()
+		var left_angle = abs(wall_normal.angle_to(Vector2.LEFT))
+		var right_angle = abs(wall_normal.angle_to(Vector2.RIGHT))
+		
+		if Input.is_action_just_pressed("move_left") and (wall_normal.is_equal_approx(Vector2.LEFT) or left_angle < 10.0):
+			velocity.x = wall_normal.x * movement_data.speed
+			velocity.y = movement_data.jump_velocity * 0.8
+		
+		if Input.is_action_just_pressed("move_right") and (wall_normal.is_equal_approx(Vector2.RIGHT) or right_angle < 10.0):
+			velocity.x = wall_normal.x * movement_data.speed
+			velocity.y = movement_data.jump_velocity * 0.8
+
 func handle_jump():
+	if is_on_floor():
+		air_jump = true
+	
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("jump_key") or Input.is_action_just_pressed("jump_key2"):
 			velocity.y = movement_data.jump_velocity
@@ -37,10 +60,20 @@ func handle_jump():
 	if not is_on_floor():
 		if Input.is_action_just_released("jump_key") and velocity.y < movement_data.jump_velocity / 2:
 			velocity.y = movement_data.jump_velocity / 2
+		
+		if movement_data.can_air_jump and Input.is_action_just_pressed("jump_key") and air_jump:
+			velocity.y = movement_data.jump_velocity * 0.8
+			air_jump = false
 
 func handle_acceleration(input_axis, delta):
+	if not is_on_floor(): return
 	if input_axis != 0:
 		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
+
+func handle_air_acceleration(input_axis, delta):
+	if is_on_floor(): return
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.air_acceleration * delta)
 
 func apply_friction(input_axis, delta):
 	if input_axis == 0 and is_on_floor():
--- scripts/resources/PlayerMovementData.gd
+++ scripts/resources/PlayerMovementData.gd
@@ -2,8 +2,10 @@ class_name PlayerMovementData
 extends Resource
 
 @export var speed = 100.0
-@export var acceleration = 800.0
-@export var friction = 1000.0
+@export var acceleration = 1000.0
+@export var friction = 2000.0
 @export var jump_velocity = -230.0
 @export var gravity_scale = 1.0
-@export var air_resistance = 200
+@export var air_resistance = 200.0
+@export var air_acceleration = 400.0
+@export var can_air_jump = false
